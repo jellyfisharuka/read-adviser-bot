f3186ed96b99cd7ede61d3e32ef58f232b3bdc93
jellyfisharuka <arukeulen@gmail.com>
2024-02-11 01:58:29 +0600

    telegram bot

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"read-adviser-bot/clients/telegram"
+)
+
+func main() {
+	const (
+		tgBotHost = "api.telegram.org"
+	)
+	//token=flags.Get(token)
+	tgClient:=telegram.New(mustToken())
+	//fetcher=fetcher.New()
+	//processor=processor.New()
+	//consumer.Start(fetcher, processor)
+}
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+	flag.Parse()
+	if *token=="" {
+		log.Fatal()
+	}
+	return *token
+}
